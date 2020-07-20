99ef1858897004b3c828792ab059315ff2de9445
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-07-20 14:15:57 -0400

    changes

--- src/main/java/com/test/example/WebConfig.java
+++ src/main/java/com/test/example/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.ContentNegotiationConfi
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebMvc
 @ComponentScan
