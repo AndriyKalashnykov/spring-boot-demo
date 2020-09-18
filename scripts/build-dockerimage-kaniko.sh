bab8a4a5ef5d8c906e18e5fff7e81705fae31a98
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-09-17 23:20:08 -0400

    removed config.json

--- scripts/build-dockerimage-kaniko.sh
+++ scripts/build-dockerimage-kaniko.sh
@@ -13,6 +13,7 @@ echo "{\"auths\":{\"$REGISTRY_URL\":{\"username\":\"$DOCKER_LOGIN\",\"password\"
 docker rm -f spring-boot-demo
 # -v ~/.docker/config.json:/kaniko/.docker/config.json:ro
 time docker run -ti --rm -v `pwd`:/workspace -v `pwd`/config.json:/kaniko/.docker/config.json:ro --env DOCKER_CONFIG=/kaniko/.docker gcr.io/kaniko-project/executor:latest --verbosity INFO --cache=true --cache-ttl=168h --cache-repo andriykalashnykov/spring-boot-demo-cache --dockerfile Dockerfile.maven-host-m2-cache --context dir:///workspace/ --destination andriykalashnykov/spring-boot-demo
+rm config.json
 #time docker run -ti --rm -v `pwd`:/workspace -v `pwd`/config.json:/kaniko/.docker/config.json:ro --env DOCKER_CONFIG=/kaniko/.docker gcr.io/kaniko-project/executor:latest --verbosity DEBUG --dockerfile Dockerfile.maven-host-m2-cache --context dir:///workspace/ --no-push
 #docker run -m500M --name spring-boot-demo -p 8080:8080 -p 8181:8081 -p 8778:8778 spring-boot-demo:latest
 
