d0585218c825162913ba1c895a901143eeb32ff6
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-09-24 10:38:41 -0400

    updated buildpacks script

--- scripts/build-dockerimage-buildpacks.sh
+++ scripts/build-dockerimage-buildpacks.sh
@@ -8,7 +8,14 @@ pushd ../
 docker image rm -f spring-boot-demo:latest
 docker rmi -f $(docker images | grep '<none>' | awk '{print $3}') 2>/dev/null
 
+# pack inspect-builder gcr.io/buildpacks/builder:v1
+#pack inspect-builder gcr.io/paketo-buildpacks/builder:base
+
+# https://github.com/paketo-buildpacks/spring-boot
+# https://github.com/spring-cloud/spring-cloud-bindings
 pack build spring-boot-demo --builder=gcr.io/paketo-buildpacks/builder:base --path=.
+
+#pack build spring-boot-demo --builder gcr.io/buildpacks/builder:v1 --path=.
 
 docker run --rm --name spring-boot-demo -p 8080:8080 -p 8181:8081 -p 8778:8778 spring-boot-demo:latest
 
